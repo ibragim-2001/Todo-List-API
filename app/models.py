397d8402bf5b5531893242e092387e0b5aa20a94
ibragim-2001 <saitamadesign7@gmail.com>
2024-10-01 20:17:28 +0300

    add urls.py, serializers, views and model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TodoItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(verbose_name='Task name', max_length=255)
+    description = models.TextField(verbose_name='Task description')
+
+    class Meta:
+        verbose_name = 'Todo item'
+        verbose_name_plural = 'Todo items'
+
+
+    def __str__(self):
+        return self.title
--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import TodoItem
+from .serializers import TodoItemSerializer
+
+
+class Todos(APIView):
+
+    def get(self, request):
+        items = TodoItem.objects.all()
+        serializer = TodoItemSerializer(items)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
